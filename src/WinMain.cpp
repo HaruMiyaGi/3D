ac583d6d70c530c3f216b7717956b105c37eecc6
HaruMiyaGi <66522128+HaruMiyaGi@users.noreply.github.com>
2020-10-19 14:59:59 +0100

    Added window class

--- src/WinMain.cpp
+++ src/WinMain.cpp
@@ -1,30 +1,12 @@
-#include <Windows.h>
+#include "Window.h"
 
-LRESULT CALLBACK WndProc(HWND, UINT, WPARAM, LPARAM);
+Window::WndClass Window::WndClass::wnd_class;
 
-int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow)
+int WINAPI WinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance, _In_ LPSTR lpCmdLine, _In_ int nCmdShow)
 {
-	WNDCLASSEX wc = { 0 };
-	wc.cbSize = sizeof(WNDCLASSEX);
-	wc.style = CS_HREDRAW | CS_VREDRAW;
-	wc.lpfnWndProc = WndProc;
-	wc.hInstance = hInstance;
-	wc.hbrBackground = (HBRUSH)COLOR_WINDOW;
-	wc.lpszClassName = "class";
-	RegisterClassEx(&wc);
 
-	RECT rect = { 0, 0, 640, 480 };
-	AdjustWindowRectEx(&rect, WS_CAPTION | WS_SYSMENU | WS_MINIMIZEBOX, FALSE, 0);
-
-	HWND hwnd = CreateWindowEx(
-		NULL, "class", "Window Title",
-		WS_OVERLAPPEDWINDOW,
-		CW_USEDEFAULT, CW_USEDEFAULT, rect.right - rect.left, rect.bottom - rect.top,
-		NULL, NULL, hInstance, NULL
-	);
-
-	ShowWindow(hwnd, nCmdShow);
-
+	Window wnd(640, 480, "uwu");
+	
 	MSG msg;
 	while (TRUE)
 	{
@@ -38,18 +20,4 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine
 	}
 
 	return 0;
-}
-
-LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
-{
-	switch (message)
-	{
-	case WM_DESTROY:
-	{
-		PostQuitMessage(0);
-	} break;
-	default:
-		return DefWindowProc(hWnd, message, wParam, lParam);
-	}
-	return DefWindowProc(hWnd, message, wParam, lParam); // Is this needed here?
 }
---
+++ src/Window.h
@@ -0,0 +1,102 @@
+#include <Windows.h>
+
+class Window
+{
+	class WndClass
+	{
+	public:
+		static const char* GetName()
+		{
+			return wnd_class.wnd_class_name;
+		}
+		static HINSTANCE GetInstance()
+		{
+			return wnd_class.hinst;
+		}
+	private:
+		WndClass()
+		{
+			WNDCLASSEX wc = { 0 };
+			wc.cbSize = sizeof(WNDCLASSEX);
+			wc.style = CS_HREDRAW | CS_VREDRAW;
+			wc.lpfnWndProc = WndProcSetup;
+			wc.hInstance = GetInstance();
+			wc.hbrBackground = (HBRUSH)COLOR_WINDOW;
+			wc.lpszClassName = GetName();
+			RegisterClassEx(&wc);
+		}
+		~WndClass()
+		{
+			UnregisterClass(GetName(), GetInstance());
+		}
+	private:
+		static constexpr const char* wnd_class_name = "class";
+		HINSTANCE hinst;
+		static WndClass wnd_class;
+	};
+public:
+	Window(int width, int height, const char* title)
+		: width(width), height(height)
+	{
+		RECT rect = { 0, 0, width, height };
+		AdjustWindowRectEx(&rect, WS_CAPTION | WS_SYSMENU | WS_MINIMIZEBOX, FALSE, 0);
+
+		hwnd = CreateWindowEx(
+			NULL, WndClass::GetName(), title,
+			WS_CAPTION | WS_MINIMIZEBOX | WS_SYSMENU,
+			CW_USEDEFAULT, CW_USEDEFAULT, rect.right - rect.left, rect.bottom - rect.top,
+			NULL, NULL, WndClass::GetInstance(), this
+		);
+
+		ShowWindow(hwnd, SW_SHOWDEFAULT);
+	}
+	~Window()
+	{
+		DestroyWindow(hwnd);
+	}
+public:
+	int GetWidth()
+	{
+		return width;
+	}
+	int GetHeight()
+	{
+		return height;
+	}
+private:
+	static LRESULT CALLBACK WndProcSetup(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam)
+	{
+		if (msg == WM_NCCREATE)
+		{
+			const CREATESTRUCTW* const pCreate = reinterpret_cast<CREATESTRUCTW*>(lParam);
+			Window* const pWnd = static_cast<Window*>(pCreate->lpCreateParams);
+			SetWindowLongPtr(hWnd, GWLP_USERDATA, reinterpret_cast<LONG_PTR>(pWnd));
+			SetWindowLongPtr(hWnd, GWLP_WNDPROC, reinterpret_cast<LONG_PTR>(&Window::WndProcThunk));
+			return pWnd->WndProc(hWnd, msg, wParam, lParam);
+		}
+		return DefWindowProc(hWnd, msg, wParam, lParam);
+	}
+	static LRESULT CALLBACK WndProcThunk(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam)
+	{
+		Window* const pWnd = reinterpret_cast<Window*>(GetWindowLongPtr(hWnd, GWLP_USERDATA));
+		return pWnd->WndProc(hWnd, msg, wParam, lParam);
+	}
+
+	LRESULT WndProc(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam)
+	{
+		switch (msg)
+		{
+		case WM_CLOSE:
+		{
+			PostQuitMessage(69);
+		} break;
+		default: return DefWindowProc(hWnd, msg, wParam, lParam);
+		}
+		return DefWindowProc(hWnd, msg, wParam, lParam);
+	}
+
+private:
+	HWND hwnd;
+	int width;
+	int height;
+};
